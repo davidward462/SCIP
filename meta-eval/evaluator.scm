9a1c032f17f69462c8fa18a55994470593998a14
David Ward <davidward462@users.noreply.github.com>
2025-10-18 22:36:39 -0700

    remove apply procedure

--- meta-eval/evaluator.scm
+++ meta-eval/evaluator.scm
@@ -413,28 +413,9 @@
 	  proc))))
 
 
+;; Apply procedure was here before
 
 
-
-;; Save reference to the oringinal 'apply' procedure
-(define apply-in-underlying-scheme apply)
-
-;; Apply
-;; 'apply' takes a procedure and a list or arguments.
-;; It handles primitive procedures and compound procedures differently, in the 'cond' statement.
-;; For the compound procedure, the environment of the given procedure must be extended so that the arguments can be bound to the parameters.
-(define (apply procedure arguments)
-  (cond ((primitive-procedure? procedure)
-	 (apply-primitive-procedure procedure arguments))
-	((compound-procedure? procedure)
-	 (eval-sequence
-	  (procedure-body procedure)
-	  (extend-environment
-	   (procedure-parameters procedure)
-	   arguments
-	   (procedure-environment procedure))))
-	(else
-	 (error "Unknown procedure type -- APPLY" procedure))))
 
 
 (define (setup-environment)
