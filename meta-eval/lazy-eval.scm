e9cc2827aed125e4de09667048fb2c147a504c6e
David Ward <davidward462@users.noreply.github.com>
2025-10-31 18:29:20 -0700

    make lazy-eval do normal order

--- meta-eval/lazy-eval.scm
+++ meta-eval/lazy-eval.scm
@@ -1,6 +1,68 @@
-
 ;; The Lazy Metacircular Evaluator
 ;; Ch 4.2
+
+;; Before we create our own mete-circular apply procedure, we must save a reference to the original Scheme 'apply'.
+(define apply-in-underlying-scheme apply)
+
+(define (actual-value exp env)
+  (force-it (eval exp env)))
+
+(define (list-of-arg-values exps env)
+  (if (no-operands? exps)
+      '()
+      (cons (actual-value (first-operand exps) env)
+	    (list-of-arg-values (rest-operands exps)
+				env))))
+
+(define (list-of-delayed-args exps env)
+  (if (no-operands? exps)
+      '()
+      (cons (delay-it (first-operand exps) env)
+	    (list-of-delayed-args (rest-operands exps)
+				  env))))
+
+(define (force-it obj)
+  (cond ((thunk? obj)
+	 (let ((result (actual-value
+			(thunk-exp obj)
+			(thunk-env obj))))
+	   (set-car! obj 'evaluated-thunk?)
+	   (set-car! (cdr obj) result) ;; replace exp with its value
+	   (set-cdr! (cdr obj) '())
+	   result))
+	((evaluated-thunk? obj)
+	 (thunk-value obj))
+	(else obj)))
+
+(define (force-it obj)
+  (cond ((thunk? obj)
+	 (let ((result (actual-value
+			(thunk-exp obj)
+			(thunk-env obj))))
+	   (set-car! obj 'evaluated-thunk?)
+	   (set-car! (cdr obj) result)
+	   (set-cdr! (cdr obj) '())
+	   result))
+	((evaluated-thunk? obj)
+	 (thunk-value obj))
+	(else obj)))
+
+;; Build a thunk out of an expression and an environment
+(define (delay-it exp env)
+  (list 'thunk exp env))
+
+(define (thunk? obj)
+  (tagged-list? obj 'thunk))
+
+(define (thunk-exp thunk) (cadr thunk))
+
+(define (thunk-env thunk) (caddr thunk))
+
+;; Create evaluated thunks for memoization
+(define (evaluated-thunk? obj)
+  (tagged-list? obj 'evaluated-thunk))
+
+(define (thunk-value evaluated-thunk) (cadr evaluated-thunk))
 
 ;; Eval
 (define (eval exp env)
@@ -27,31 +89,28 @@
           env))
         ((cond? exp) 
          (eval (cond->if exp) env))
-        ((application? exp)
-         (apply (eval (operator exp) env)
-                (list-of-values 
-                 (operands exp) 
-                 env)))
+	;; edited below
+	((application? exp)
+	 (apply (actual-value (operator exp) env)
+		(operands exp)
+		env))
         (else
          (error "Unknown expression 
                  type: EVAL" exp))))
 
-;; Before we create our own mete-circular apply procedure, we must save a reference to the original Scheme 'apply'.
-(define apply-in-underlying-scheme apply)
-
 ;; Appy 
-(define (apply procedure arguments)
+(define (apply procedure arguments env)
   (cond ((primitive-procedure? procedure)
          (apply-primitive-procedure 
           procedure 
-          arguments))
+          (list-of-arg-values arguments env))) ;; changed
         ((compound-procedure? procedure)
          (eval-sequence
            (procedure-body procedure)
            (extend-environment
              (procedure-parameters 
               procedure)
-             arguments
+             (list-of-delayed-args arguments env) ;; changed
              (procedure-environment 
               procedure))))
         (else
@@ -71,11 +130,11 @@
 
 
 ;; Conditionals
+;; changed here
 (define (eval-if exp env)
-  (if (true? (eval (if-predicate exp) env))
+  (if (true? (actual-value (if-predicate exp) env))
       (eval (if-consequent exp) env)
       (eval (if-alternative exp) env)))
-
 
 ;; Sequences
 (define (eval-sequence exps env)
@@ -365,7 +424,12 @@
   (list (list 'car car)
         (list 'cdr cdr)
         (list 'cons cons)
-        (list 'null? null?))) ;; TODO: add more primitives here
+        (list 'null? null?)
+	(list '+ +)
+	(list '- -)
+	(list '* *)
+	(list '/ /)
+	(list '= =)))
 
 (define (primitive-procedure-names)
   (map car primitive-procedures))
@@ -375,6 +439,7 @@
          (list 'primitive (cadr proc)))
        primitive-procedures))
 
+;; TODO: Does this need to take an environment somehow?
 (define (apply-primitive-procedure proc args)
   (apply-in-underlying-scheme
    (primitive-implementation proc) args))
@@ -392,15 +457,14 @@
 
 ;; Driver loop and display
 
-(define input-prompt  ";;; M-Eval input:")
-(define output-prompt ";;; M-Eval value:")
+(define input-prompt  ";;; L-Eval input:")
+(define output-prompt ";;; L-Eval value:")
 
 (define (driver-loop)
   (prompt-for-input input-prompt)
   (let ((input (read)))
-    (let ((output 
-           (eval input 
-                 the-global-environment)))
+    (let ((output
+	   (actual-value input the-global-environment)))
       (announce-output output-prompt)
       (user-print output)))
   (driver-loop))
